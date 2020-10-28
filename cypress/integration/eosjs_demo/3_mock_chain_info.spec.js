f9ea2bc69ae2c85638fdc420a32683c2ff233717
Joe <josephedwardwork@gmail.com>
2020-10-28 00:07:34 -0400

    cypress tests

--- cypress/integration/eosjs_demo/3_mock_chain_info.spec.js
+++ cypress/integration/eosjs_demo/3_mock_chain_info.spec.js
@@ -12,22 +12,22 @@ describe("passes mock data about current blockchain state, get_info call", () =>
       console.log("chainInfo : ", chainInfo);
       // cy.get(".ArwesSounds\(Frame\)-root-0-1-91 > .ArwesSounds\(Frame\)-box-0-1-16 > .ArwesSounds\(Frame\)-children-0-1-17 > .Table-root-0-1-99 > div > table > tbody > tr > td")
       // // .invoke('text')
-      cy.contains(chainInfo.server_version).should("exist");
-      cy.contains(chainInfo.chain_id).should("exist");
-      cy.contains(chainInfo.head_block_num).should("exist");
-      cy.contains(chainInfo.last_irreversible_block_num).should("exist");
-      cy.contains(chainInfo.last_irreversible_block_id).should("exist");
-      cy.contains(chainInfo.head_block_id).should("exist");
-      cy.contains(chainInfo.head_block_time).should("exist");
-      cy.contains(chainInfo.head_block_producer).should("exist");
-      cy.contains(chainInfo.virtual_block_cpu_limit).should("exist");
-      cy.contains(chainInfo.virtual_block_net_limit).should("exist");
-      cy.contains(chainInfo.block_cpu_limit).should("exist");
-      cy.contains(chainInfo.block_net_limit).should("exist");
-      cy.contains(chainInfo.server_version_string).should("exist");
-      cy.contains(chainInfo.fork_db_head_block_num).should("exist");
-      cy.contains(chainInfo.fork_db_head_block_id).should("exist");
-      cy.contains(chainInfo.server_full_version_string).should("exist");
+      cy.contains(chainInfo.server_version).invoke('text').should('exist');
+      cy.contains(chainInfo.chain_id).invoke('text').should('exist');
+      cy.contains(chainInfo.head_block_num).invoke('text').should('exist');
+      cy.contains(chainInfo.last_irreversible_block_num).invoke('text').should('exist');
+      cy.contains(chainInfo.last_irreversible_block_id).invoke('text').should('exist');
+      cy.contains(chainInfo.head_block_id).invoke('text').should('exist');
+      cy.contains(chainInfo.head_block_time).invoke('text').should('exist');
+      cy.contains(chainInfo.head_block_producer).invoke('text').should('exist');
+      cy.contains(chainInfo.virtual_block_cpu_limit).invoke('text').should('exist');
+      cy.contains(chainInfo.virtual_block_net_limit).invoke('text').should('exist');
+      cy.contains(chainInfo.block_cpu_limit).invoke('text').should('exist');
+      cy.contains(chainInfo.block_net_limit).invoke('text').should('exist');
+      cy.contains(chainInfo.server_version_string).invoke('text').should('exist');
+      cy.contains(chainInfo.fork_db_head_block_num).invoke('text').should('exist');
+      cy.contains(chainInfo.fork_db_head_block_id).invoke('text').should('exist');
+      cy.contains(chainInfo.server_full_version_string).invoke('text').should('exist');
     });
   });
 
